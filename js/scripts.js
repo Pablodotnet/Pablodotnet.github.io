cdce333a022ac6eec0fd0c9e551632701a09f010
Luis Pablo <luis.pablo.lopez.lopez@uabc.edu.mx>
2018-10-17 12:37:09 -0700

    fix: mobile menu now works as expected

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 /*
+*   Author: beshleyua
+*   Author URL: http://themeforest.net/user/beshleyua
+*/
+
+
+/*
 	Preloader
 */
 
@@ -33,36 +39,39 @@ $(function () {
 	$('.top-menu').on('click', 'a', function(){
 
 		/* vars */
+		var width = $(window).width();
 		var id = $(this).attr('href');
 		var h = parseFloat($(id).offset().top);
 		var card_item = $(id);
 		var menu_items = $('.top-menu li');
 		var menu_item = $(this).closest('li');
 		var d_lnk = $('.lnks .lnk.discover');
-		
-		/* if desktop */
-		if(!menu_item.hasClass('active') & (width > 1023) & $('#home-card').length) {
 
-			/* close card items */
-			menu_items.removeClass('active');
-			container.find(card_items).removeClass('animated '+animation_in);
+		if((width >= 1024)) {
+			
+			/* if desktop */
+			if(!menu_item.hasClass('active') & (width > 1023) & $('#home-card').length) {
 
-			if($(container).hasClass('opened')) {
-				container.find(card_items).addClass('animated '+animation_out);
+				/* close card items */
+				menu_items.removeClass('active');
+				container.find(card_items).removeClass('animated '+animation_in);
+
+				if($(container).hasClass('opened')) {
+					container.find(card_items).addClass('animated '+animation_out);
+				}
+
+				/* open card item */
+				menu_item.addClass('active');
+				container.addClass('opened');
+				container.find(card_item).removeClass('animated '+animation_out);
+				container.find(card_item).addClass('animated '+animation_in);
+				
+				$(card_items).addClass('hidden');
+				
+				$(card_item).removeClass('hidden');
+				$(card_item).addClass('active');
 			}
-
-			/* open card item */
-			menu_item.addClass('active');
-			container.addClass('opened');
-			container.find(card_item).removeClass('animated '+animation_out);
-			container.find(card_item).addClass('animated '+animation_in);
-			
-			$(card_items).addClass('hidden');
-			
-			$(card_item).removeClass('hidden');
-			$(card_item).addClass('active');
 		}
-		
 		/* if mobile */
 		if((width < 1024) & $('#home-card').length) {
 
@@ -71,8 +80,44 @@ $(function () {
 				scrollTop: h - 76
 			}, 800);
 		}
-		
 		return false;
+	});
+
+	$(window).on('resize', function(){
+		var width = $(window).width();
+		var height = $(window).height();
+
+		if((width < 1024)) {
+			$('.card-inner').removeClass('hidden');
+			$('.card-inner').removeClass('fadeOutLeft');
+			$('.card-inner').removeClass('rotateOutUpLeft');
+			$('.card-inner').removeClass('rollOut');
+			$('.card-inner').removeClass('jackOutTheBox');
+			$('.card-inner').removeClass('fadeOut');
+			$('.card-inner').removeClass('fadeOutUp');
+			$('.card-inner').removeClass('animated');
+
+			$(window).on('scroll', function(){
+				var scrollPos = $(window).scrollTop();
+				$('.top-menu ul li a').each(function () {
+					var currLink = $(this);
+					var refElement = $(currLink.attr("href"));
+					if (refElement.offset().top - 76 <= scrollPos) {
+						$('.top-menu ul li').removeClass("active");
+						currLink.closest('li').addClass("active");
+					}
+				});
+			});
+
+			$('.card-inner .card-wrap').slimScroll({destroy: true});
+			$('.card-inner .card-wrap').attr('style', '');
+		}
+		else {
+			$($('.top-menu li.active a').attr('href')).addClass('active');
+			$('.card-inner .card-wrap').slimScroll({
+				height: '570px'
+			});
+		}
 	});
 	
 	
